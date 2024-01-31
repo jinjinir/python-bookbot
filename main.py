01ede5d0fbe7eff262a3b5ccf1626ad94cd74bce
Daryl Kevin <daryl.galvez@outlook.com>
2024-01-31 13:37:01 +0000

    code: add main function

--- main.py
+++ main.py
@@ -3,3 +3,13 @@
 def read_book(book):
     with open(book) as f:
         return f.read()
+
+
+def main():
+    book_loc = "books/frankenstein.txt"
+    book_text = read_book(book_loc)
+    print(book_text)
+
+
+if __name__ == "__main__":
+    main()
